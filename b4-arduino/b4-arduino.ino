b5ac858eac3e4dee1fbb161e5835f7c43e158e5b
bobboteck <bobboteck@gmail.com>
2018-10-01 12:31:30 +0000

    promoted to a stable version the Arduino Test code with a single US sensor

--- b4-arduino/b4-arduino.ino
+++ b4-arduino/b4-arduino.ino
@@ -1,13 +1,13 @@
 /******************************************************************************
  * B4-Arduino
  * 
- * Created 17/09/2018 - By Roberto D'Amico
+ * Created 01/09/2018 - By Roberto D'Amico
  * 
  * Description: this program measures the distance with the 3 US sensors and 
  * publishes the values as ROS messages
  * 
  * How to use: For more details on how to operate refer to the repository Wiki
- * 
+ * at https://github.com/bobboteck/B4/wiki/ROS-Range-HC-SR04
  * 
  * 
  * MIT License
@@ -32,6 +32,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  *****************************************************************************/ 
+
 #include <NewPing.h>
 #include <ros.h>
 #include <ros/time.h>
@@ -39,67 +40,40 @@
 
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
 
+char frameid[] = "uSonarRange_topic";
 long range_time;
 
-NewPing uSonarLeft = NewPing(2,3,MAX_DISTANCE);
-NewPing uSonarCenter = NewPing(4,5,MAX_DISTANCE);
-NewPing uSonarRight = NewPing(6,7,MAX_DISTANCE);
+NewPing uSonar = NewPing(2,3,MAX_DISTANCE);
 
 
-// ROS - Node handle
+//ROS
 ros::NodeHandle nh;
-// ROS - Message type definition
-sensor_msgs::Range uSonarLeftRangeMsg;
-// ROS - Topics defintion
-ros::Publisher pub_rangeLeft("topicSonarRangeLeft", &uSonarLeftRangeMsg);
-ros::Publisher pub_rangeCenter("topicSonarRangeCenter", &uSonarCenterRangeMsg);
-ros::Publisher pub_rangeRight("topicSonarRangeRight", &uSonarRightRangeMsg);
+
+sensor_msgs::Range uSonarRangeMsg;
+ros::Publisher pub_range("uSonarRange_topic", &uSonarRangeMsg);
 
 void setup() 
 {
   //ROS
   nh.initNode();
-  nh.advertise(pub_rangeLeft);
-  nh.advertise(pub_rangeCenter);
-  nh.advertise(pub_rangeRight);
+  nh.advertise(pub_range);
 
-  uSonarLeftRangeMsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  uSonarLeftRangeMsg.header.frame_id = "frameSonarRangeLeft";
-  uSonarLeftRangeMsg.field_of_view = 0.523599f;		//30 degrees in radians
-  uSonarLeftRangeMsg.min_range = 0.05;    			// Meters
-  uSonarLeftRangeMsg.max_range = 2;       			// Meters
-  
-  uSonarCenterRangeMsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  uSonarCenterRangeMsg.header.frame_id = "frameSonarRangeCenter";
-  uSonarCenterRangeMsg.field_of_view = 0.523599f;	//30 degrees in radians
-  uSonarCenterRangeMsg.min_range = 0.05;    		// Meters
-  uSonarCenterRangeMsg.max_range = 2;       		// Meters
-  
-  uSonarRightRangeMsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  uSonarRightRangeMsg.header.frame_id = "frameSonarRangeRight";
-  uSonarRightRangeMsg.field_of_view = 0.523599f;	//30 degrees in radians
-  uSonarRightRangeMsg.min_range = 0.05;    			// Meters
-  uSonarRightRangeMsg.max_range = 2;       			// Meters
+  uSonarRangeMsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  uSonarRangeMsg.header.frame_id = frameid;
+  uSonarRangeMsg.field_of_view = 0.523599f; //30 degrees in radians
+  uSonarRangeMsg.min_range = 0.05;    // Meters
+  uSonarRangeMsg.max_range = 2;       // Meters
 }
 
 void loop() 
 {
   if(millis() >= range_time)
   {
-    uSonarLeftRangeMsg.range = (float)uSonarLeft.ping_cm() / 100;
-    uSonarLeftRangeMsg.header.stamp = nh.now();
-    pub_rangeLeft.publish(&uSonarLeftRangeMsg);
-	
-	uSonarCenterRangeMsg.range = (float)uSonarCenter.ping_cm() / 100;
-    uSonarCenterRangeMsg.header.stamp = nh.now();
-    pub_rangeCenter.publish(&uSonarCenterRangeMsg);
-	
-	uSonarRightRangeMsg.range = (float)uSonarRight.ping_cm() / 100;
-    uSonarRightRangeMsg.header.stamp = nh.now();
-    pub_rangeRight.publish(&uSonarRightRangeMsg);
-	
+    uSonarRangeMsg.range = (float)uSonar.ping_cm() / 100;
+    uSonarRangeMsg.header.stamp = nh.now();
+    pub_range.publish(&uSonarRangeMsg);
     range_time = millis() + 500;
   }
 
-  nh.spinOnce();
+  nh.spinOnce();	// Is correct here? Or call only when have a new data?
 }
